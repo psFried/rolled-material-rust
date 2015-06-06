4a0ecb613c8a49f03405eb2ba6131b3a3c977e8b
pfried <philipsfried@gmail.com>
2015-06-06 16:33:22 -0400

    changed calc_total_length to accept Length structs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 mod units;
 
+// use units::units;
+use units::units::METERS;
+use units::units::Length;
+
 const EPSILON: f64 = 0.00001f64;
 
 fn main() {
@@ -10,34 +14,23 @@ fn main() {
 }
 
 fn get_output(args: Vec<String>) -> String {
-    if args.len() == 4 {
-        let inside_diameter_mm = args[1].parse::<f64>().ok().unwrap();
-        let outside_diameter_mm = args[2].parse::<f64>().ok().unwrap();
-        let thickness_mm = args[3].parse::<f64>().ok().unwrap();
-
-        let length = calc_total_length_meters(inside_diameter_mm / 2f64, outside_diameter_mm / 2f64, thickness_mm);
-        println!("length= {}", length);
-        format!("{}", length).to_string()
-    } else {
-        "must supply 3 arguments".to_string()
-    }
+    "not done yet".to_string()
 }
 
 // fn format_output(length: f64) -> String {
 //
 // }
 
-fn calc_total_length_meters(inside_radius_mm: f64, outside_radius_mm: f64, thickness_mm: f64) -> f64 {
-    let mut current_radius = inside_radius_mm;
-
-    let mut total_length_mm = 0f64;
-
-    while current_radius < outside_radius_mm {
-        total_length_mm += determine_circumfrence(current_radius);
-        current_radius += thickness_mm;
+fn calc_total_length(inside_diameter: &Length, outside_diameter: &Length, thickness: &Length) -> Length {
+    let mut current_diameter = inside_diameter.convert_to(METERS).value;
+    let od_m = outside_diameter.convert_to(METERS).value;
+    let thickness_adder = 2f64 * thickness.convert_to(METERS).value;
+    let mut length = 0f64;
+    while current_diameter <= od_m {
+        length += determine_circumfrence(current_diameter);
+        current_diameter += thickness_adder;
     }
-
-    total_length_mm / 1000f64
+    Length{value: length, unit: METERS }
 }
 
 fn determine_circumfrence(diameter: f64) -> f64 {
@@ -52,8 +45,14 @@ fn test_determine_circumference() {
 }
 
 #[test]
-fn test_calc_total_length_meters() {
-    // let len = &units::units::Length { value: 5f64, unit: units::units::Meters()};
+fn test_calc_total_length() {
+    let id = Length{value: 0.1f64, unit: METERS};
+    let od = Length{value: 1.1f64, unit: METERS};
+    let thickness = Length{value: 0.01f64, unit: METERS};
+
+    let result = calc_total_length(&id, &od, &thickness);
+    println!("result= {:?}", result);
+    println!("do yo uhear me?");
 
 }
 
